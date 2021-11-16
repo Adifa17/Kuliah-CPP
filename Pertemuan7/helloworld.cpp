15c173288d5d76bd0cc1789d5a810bca98379fd0
Adifa17 <91317969+Adifa17@users.noreply.github.com>
2021-11-16 15:23:59 +0700

    memberi komen

--- Pertemuan7/helloworld.cpp
+++ Pertemuan7/helloworld.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello World!";
-    std::cin.get();
+    std::cout << "Hello World!"; // Untuk Output berupa Hello World
+    std::cin.get(); // Menampilkan output dalam format exe (Application)
     return 0;
 }
